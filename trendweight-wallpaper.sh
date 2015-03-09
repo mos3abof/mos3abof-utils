15600a12f72c78405a2a882f543ef79f0d845dd6
Mosab Ahmad <mahmad@yubb-software.com>
2015-03-09 14:39:40 +0200

    A simple bash script to update my desktop backgound with my trendweight graph

---
+++ trendweight-wallpaper.sh
@@ -0,0 +1,4 @@
+cd /home/mosab/Pictures/trendweight/
+wget https://trendweight.com/u/b283609cdcdc41/chart/1y.png
+gsettings set org.gnome.desktop.background picture-uri "file:///home/mosab/Pictures/trendweight/1y.png"
+gsettings set org.gnome.desktop.background picture-options "centered"
