af0b6b06fc39dedffd6254ba7812be53ebfd6ee9
Mosab Ahmad <mahmad@yubb-software.com>
2015-03-09 18:51:49 +0200

    Updated script to overwrite existing image, if it exists, rather than saing it with a different name

--- trendweight-wallpaper.sh
+++ trendweight-wallpaper.sh
@@ -1,4 +1,3 @@
-cd /home/mosab/Pictures/trendweight/
-wget https://trendweight.com/u/b283609cdcdc41/chart/1y.png
+wget https://trendweight.com/u/b283609cdcdc41/chart/1y.png -O /home/mosab/Pictures/trendweight/1y.png
 gsettings set org.gnome.desktop.background picture-uri "file:///home/mosab/Pictures/trendweight/1y.png"
 gsettings set org.gnome.desktop.background picture-options "centered"
