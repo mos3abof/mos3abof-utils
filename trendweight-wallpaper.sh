bd9e66d86301592982b6416edfadaa284356e9c3
Mosab Ahmad <mahmad@yubb-software.com>
2015-04-01 12:32:20 +0200

    Need to delete the picture before downloading a new one

--- trendweight-wallpaper.sh
+++ trendweight-wallpaper.sh
@@ -1,3 +1,4 @@
+rm /home/mosab/Pictures/trendweight/1y.png
 wget https://trendweight.com/u/b283609cdcdc41/chart/1y.png -O /home/mosab/Pictures/trendweight/1y.png
 gsettings set org.gnome.desktop.background picture-uri "file:///home/mosab/Pictures/trendweight/1y.png"
 gsettings set org.gnome.desktop.background picture-options "centered"
